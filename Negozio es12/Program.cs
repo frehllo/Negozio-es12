b61987fc1fdaa0177c55354107f99ca1f13604f3
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:11:26 +0200

    Sconto sui primi 300 e sul resto.

--- Negozio es12/Program.cs
+++ Negozio es12/Program.cs
@@ -15,7 +15,7 @@ namespace Negozio_es12
             {
                 sconto = (300 / 100) * 10;
                 double resto = tot - 300;
-                Console.WriteLine("Applico uno sconto...");
+                Console.WriteLine("Applico uno sconto... ");
                 sconto20 = (resto / 100) * 20;
                 prezzofinale = 270 + resto - sconto20;
                 Console.WriteLine($"Importo scontato da pagare {prezzofinale} applicando uno sconto di {sconto} euro sui primi 300 euro e uno sconto di {sconto20} euro sul resto.");
